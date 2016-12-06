b25cb85f1c191c50a383fced3e60e3143a146756
dum0nt <joel.dumont@bluewin.ch>
2016-12-06 18:00:12 +0100

    empty style.css added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 /**
  * Created by joelm on 04.12.2016.
  */
+
+$(document).ready(function() {
+    // page is now ready, initialize the calendar...
+
+    $('#calendar').fullCalendar({
+        // put your options and callbacks here
+    })
+
+});
